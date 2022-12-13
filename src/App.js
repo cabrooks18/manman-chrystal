40ce0cd644093aa67607246cc12fd0ad057836ec
cabrooks18 <114538088+cabrooks18@users.noreply.github.com>
2022-12-12 20:56:22 -0500

    2 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import Nav from './Nav';
 import Cart from './Cart';
 import Menu from './Menu';
 import Footer from './Footer';
+import Calendar from './Calendar';
+import Carousel from './Carousel';
+
 
 function App() {
   return (
     <div>
       <Nav />
+      <Calendar />
       <Cart />
       <Menu />
+      <Carousel />
       <Footer />
+      
 
       {/* <Hero />
         <Features />
